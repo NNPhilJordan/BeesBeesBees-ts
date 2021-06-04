54a6dc2ad2989954b5e79d7a4ecec4b56b07f01f
NNPhilJordan <phil.jordan@nelnet.net>
2021-06-04 10:39:58 -0600

    'Completed Bee Exercises'

--- src/ForagerBee.ts
+++ src/ForagerBee.ts
@@ -1,2 +1,15 @@
-class ForagerBee {
+class ForagerBee extends Bee 
+{
+    canFly : boolean = true;
+    treasureChest: Array<string> = [];
+    constructor() {
+        super();
+        this.age = 10;
+        this.job = 'find pollen';
+               
+     } 
+
+     forage (treasueAdd:string) {
+        this.treasureChest.push(treasueAdd)  ;
+    }
 };
--- src/HoneyMakerBee.ts
+++ src/HoneyMakerBee.ts
@@ -1,1 +1,15 @@
-class HoneyMakerBee extends Bee { };
+class HoneyMakerBee extends Bee { 
+    honeyPot:number = 0;
+    constructor() {
+        super();
+        this.age = 10;
+        this.color = 'yellow';
+        this.job = 'make honey';
+     } 
+     makeHoney () {
+         this.honeyPot += 1;
+     }
+     giveHoney () {
+        this.honeyPot -= 1;
+    }
+};
